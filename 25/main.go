5a75ecd3409c4f267e911b68a9829be1465a126b
Marcos Vinicius <vinicius_siqueira-1997@hotmail.com>
2024-10-21 11:57:23 -0300

    feat: 25

--- 25/main.go
+++ 25/main.go
@@ -11,33 +11,92 @@ type ListNode struct {
 	Next *ListNode
 }
 
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) Push(item int) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() {
+	if s == nil {
+		return
+	}
+	s.items = s.items[:len(s.items)-1]
+}
+
+func (s *Stack) Top() (int, error) {
+	if s == nil {
+		return 0, fmt.Errorf("stack is empty")
+	}
+
+	return s.items[len(s.items)-1], nil
+}
+
+func (s *Stack) IsEmpty() bool {
+	if len(s.items) == 0 {
+		return true
+	}
+	return false
+}
+
+func (s *Stack) Clear() {
+	if s == nil {
+		return
+	}
+	s.items = []int{}
+}
+
+func (s *Stack) Print() {
+	for _, item := range s.items {
+		fmt.Print(item, " ")
+	}
+	fmt.Println()
+}
+
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	aux := &ListNode{0, head}
-	current := aux
-	// i := 0
+	var dummy = &ListNode{}
+	var aux = dummy
+	stack := &Stack{}
 
 	for head != nil {
+		var dummyAux = &ListNode{}
+		var lstAux = dummyAux
 		for j := 0; j < k; j++ {
-			current = current.Next
+			stack.Push(head.Val)
+			dummyAux.Next = &ListNode{Val: head.Val}
+			dummyAux = dummyAux.Next
+			head = head.Next
+
+			if head == nil && j+1 != k {
+				dummy.Next = lstAux.Next
+				stack.Clear()
+				break
+			}
 		}
-		head = head.Next
+
+		for !stack.IsEmpty() {
+			v, _ := stack.Top()
+			dummy.Next = &ListNode{Val: v}
+			dummy = dummy.Next
+			stack.Pop()
+		}
+
+		stack.Clear()
 	}
-	return nil
+
+	return aux.Next
 }
 
 func main() {
 	list := ListNode{Val: 1, Next: &ListNode{
 		Val: 2,
-		Next: &ListNode{
-			Val: 3,
-			Next: &ListNode{
-				Val: 4,
-				Next: &ListNode{
-					Val:  5,
-					Next: nil,
-				},
-			},
-		},
 	}}
-	fmt.Println("Result", reverseKGroup(&list, 2))
+	revert := reverseKGroup(&list, 2)
+
+	for revert != nil {
+		fmt.Println("KKK", revert.Val)
+		revert = revert.Next
+	}
 }
